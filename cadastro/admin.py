ffe86c014ee3b53aa6172e112547f014fe9ed1c3
William Santos <Willsantos.edf@gmail.com>
2023-01-24 00:25:58 -0300

    Criando  modelos para o módulo de cadastro.

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Campu, Atividade
 # Register your models here.
+
+admin.site.register(Campu)
+admin.site.register(Atividade)
+
--- cadastro/models.py
+++ cadastro/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Campo(models.Model):
+class Campu(models.Model):
     nome = models.CharField(max_length=50,)
     descricao = models.CharField(max_length=150, verbose_name="Descrição")
 
@@ -9,11 +9,11 @@ class Campo(models.Model):
         return f'{self.nome} ({self.descricao})'
 
 class Atividade(models.Model):
-    numero = models.CharField(verbose_name='Número')
+    numero = models.IntegerField(verbose_name='Número')
     descricao = models.CharField(max_length=150, verbose_name='Descrição')
     pontos = models.DecimalField(decimal_places=1, max_digits=4)
     detalhes = models.CharField(max_length=100)
-    campo = models.ForeignKey(Campo, on_delete=models.CASCADE)
+    campo = models.ForeignKey(Campu, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.nome} ({self.campo.nome})'
+        return f'{self.numero} - {self.descricao} ({self.campo.nome})'
