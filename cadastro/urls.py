96309f35ce05f4842184eefd60272425a0951bfb
William Santos <Willsantos.edf@gmail.com>
2023-01-24 22:07:32 -0300

    Criando ostemplates do createView e UpdateView.

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import CampuCreate, AtividadeCreate
 from .views import CampuUpdate, AtividadeUpdate
+from .views import CampuDelete, AtividadeDelete
+from .views import CampuList, AtividadeList
 
 
 urlpatterns = [
@@ -8,6 +10,12 @@ urlpatterns = [
     path('cadastrar/atividade/', AtividadeCreate.as_view(), name='cadastrar-atividade'),
 
     path('editar/campo/<int:pk>/', CampuUpdate.as_view(), name='editar-campo'),
-    path('editar/atividade/<int:pk>/', CampuUpdate.as_view(), name='editar-atividade'),
+    path('editar/atividade/<int:pk>/', AtividadeUpdate.as_view(), name='editar-atividade'),
+
+    path('excluir/campo/<int:pk>/', CampuDelete.as_view(), name='excluir-campo'),
+    path('excluir/atividade/<int:pk>/', AtividadeDelete.as_view(), name='excluir-atividade'),
+
+    path('listar/campo/', CampuList.as_view(), name='listar-campo'),
+    path('listar/atividade/', AtividadeList.as_view(), name='listar-atividade'),
     
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 from .models import Campu, Atividade
 from django.urls import reverse_lazy
 
@@ -32,3 +32,26 @@ class AtividadeUpdate(UpdateView):
     fields = ['numero','descricao','pontos', 'detalhes', 'campo']
     template_name ='cadastro/form.html'
     success_url = reverse_lazy ('index')   
+
+
+################# Delete ################
+
+class CampuDelete(DeleteView):
+    model = Campu
+    template_name = 'cadastro/form-excluir.html'
+    success_url = reverse_lazy('index')
+
+class AtividadeDelete(DeleteView):
+    model = Atividade
+    template_name = 'cadastro/form-excluir.html'
+    success_url = reverse_lazy('index')
+
+################# List ################
+
+class CampuList(ListView):
+    model = Campu
+    template_name = 'cadastro/lista/campo.html'
+
+class AtividadeList(ListView):
+    model = Atividade
+    template_name = 'cadastro/lista/atividade.html'
